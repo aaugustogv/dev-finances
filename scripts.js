9fd79203e0944818df9862bb62d29b49e1b1a5c3
Andre Viana <code.andre3@gmail.com>
2021-05-12 17:19:23 -0300

    my comments about Utils/Moeda

--- scripts.js
+++ scripts.js
@@ -47,26 +47,25 @@ const Transaction = {
 //Pegar os dados do HTML e substituir pelo os do JS
 const DOM = {
 
-    transactionsContainer: document.querySelector('#data-table tbody'), //Seleciona o id #Data-table dentro o
-    // tbody
+    transactionsContainer: document.querySelector('#data-table tbody'), 
+    
 
     addTransaction(transaction,index) {
-        const tr = document.createElement('tr') // cria o elemento tr (no html)
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction) //ascessa no objeto DOM o innerHTMLTransaction
-        //na qual o transaction tem a estrutura HTML das Tabelas (transaction)
+        const tr = document.createElement('tr') 
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction) 
 
-        DOM.transactionsContainer.appendChild(tr)//Aqui ele ira adicionar o tr com a função appendChild(tr)
-        // para colocar em volta de todo innerHTMLTransaction
+        DOM.transactionsContainer.appendChild(tr)
 
     },
 
-    innerHTMLTransaction(transaction) { // o innerHTMLTransaction onde é colocado a estrutura HTML
+    innerHTMLTransaction(transaction) { 
+        const CSSclass = transaction.amount > 0 ? "income" : "expenses"
 
-
-        //esses ${}, são para colocar as variáveis das transaction nos seus determinados locais
+        const amount = Utils.formatCurrency(transaction.amount)
+        
         const html = `
             <td class="description">${transaction.description}</td> 
-            <td class="expenses">${transaction.amount}</td>
+            <td class="${CSSclass}">${transaction.amount}</td>
             <td class="date">${transaction.date}</td>
             <td> 
                 <img src="./assets/minus.svg" alt="Remover Transação">
@@ -77,9 +76,24 @@ const DOM = {
     }
 }
 
-DOM.addTransaction(transactions[0])
-DOM.addTransaction(transactions[1])
-DOM.addTransaction(transactions[2])
+const Utils = {
+    formatCurrency(value) { //Vamos entender como funciono os números do .amount
+        const signal = Number(value) < 0 ? "-" : "" // se menor 1ue 0 adicione - (menos)
 
-transactions.forEach(function(transaction) {})
+        value = String(value).replace(/\D/g,"") // se for uma String, coloque nada
+
+        value = Number(value) / 100 // atualizei o value para número de novo
+
+        value = value.toLocaleString("pt-BR", { //e aqui utilizamos o .toLocaleString para aplicar o tipo de moeda em que estamos trabalhando
+            style: "currency", //estilo moeda
+            currency: "BRL"
+        })
+
+        console.log(signal + value)
+    }
+}
+
+transactions.forEach(function(transaction) {
+    DOM.addTransaction(transaction)
+})
 
